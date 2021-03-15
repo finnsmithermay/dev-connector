e3e8834c577db1395a0676356cda62f471c8eedd
Finn <finnsmithermay@gmail.com>
2021-03-15 16:01:56 +1300

    commit message from terminal

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
+const path = require('path')
 
 connectDB();
 
@@ -10,9 +11,10 @@ connectDB();
 
 app.use(express.json({extended: false}));
 
-const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('API running'));
+//app.get('/', (req, res) => res.send('API running'));
+
+
 
 //define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -20,6 +22,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+//serve static assets in productin
+
+if(process.env.NODE_ENV === 'production'){
+    //set static filder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html' ));
+    })
+}
+
+const PORT = process.env.PORT || 5000;
 
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
